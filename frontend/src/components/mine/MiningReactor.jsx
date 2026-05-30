532bd6fe6a4a10b291f5a81a61a2158ea769f221
abat ubbiev <aaubbiev@gmail.com>
2026-05-30 18:45:46 +0200

    Animate sapphire mining crystal

--- frontend/src/components/mine/MiningReactor.jsx
+++ frontend/src/components/mine/MiningReactor.jsx
@@ -3,16 +3,14 @@ import { sapphireDisplay, sapphireNumber } from "../../lib/sapphireFormat.js";
 import crystalImg from "../../assets/sapphire-crystal.png";
 
 const FLOATING_SHARDS = [
-  { id: "s1", left: "6%", top: "18%", delay: "0s", size: 12, rotate: -18, dur: "7s" },
-  { id: "s2", left: "88%", top: "24%", delay: "1.4s", size: 10, rotate: 22, dur: "8.5s" },
-  { id: "s3", left: "14%", top: "42%", delay: "2.2s", size: 8, rotate: -8, dur: "6.5s" },
-  { id: "s4", left: "82%", top: "38%", delay: "0.8s", size: 11, rotate: 14, dur: "9s" },
-  { id: "s5", left: "22%", top: "62%", delay: "3.1s", size: 9, rotate: -24, dur: "7.8s" },
-  { id: "s6", left: "76%", top: "58%", delay: "1.9s", size: 10, rotate: 10, dur: "8.2s" },
-  { id: "s7", left: "4%", top: "72%", delay: "2.6s", size: 7, rotate: 16, dur: "6.8s" },
-  { id: "s8", left: "92%", top: "68%", delay: "0.5s", size: 9, rotate: -12, dur: "7.4s" },
-  { id: "s9", left: "32%", top: "12%", delay: "3.8s", size: 8, rotate: 6, dur: "9.5s" },
-  { id: "s10", left: "68%", top: "14%", delay: "2.4s", size: 7, rotate: -20, dur: "8.8s" },
+  { id: "s1", left: "8%", top: "20%", delay: "0s", size: 10, rotate: -18, dur: "7s" },
+  { id: "s2", left: "86%", top: "26%", delay: "1.4s", size: 8, rotate: 22, dur: "8.5s" },
+  { id: "s3", left: "16%", top: "44%", delay: "2.2s", size: 7, rotate: -8, dur: "6.5s" },
+  { id: "s4", left: "80%", top: "40%", delay: "0.8s", size: 9, rotate: 14, dur: "9s" },
+  { id: "s5", left: "24%", top: "64%", delay: "3.1s", size: 7, rotate: -24, dur: "7.8s" },
+  { id: "s6", left: "74%", top: "60%", delay: "1.9s", size: 8, rotate: 10, dur: "8.2s" },
+  { id: "s7", left: "6%", top: "74%", delay: "2.6s", size: 6, rotate: 16, dur: "6.8s" },
+  { id: "s8", left: "90%", top: "70%", delay: "0.5s", size: 7, rotate: -12, dur: "7.4s" },
 ];
 
 export default function MiningReactor({
@@ -70,14 +68,14 @@ export default function MiningReactor({
 
   return (
     <section className="sapphire-reactor sapphire-reactor--hero" aria-label="Mining reactor">
+      <div className="sapphire-reactor__head">
+        <span className="sapphire-label">Total Shards</span>
+        <span className="sapphire-reactor__shards sapphire-mono">{sapphireNumber(profile?.shards)}</span>
+      </div>
+
       <div
         className={`sapphire-reactor__stage ${active ? "sapphire-reactor__stage--active" : ""} ${disabled ? "sapphire-reactor__stage--disabled" : ""}`}
       >
-        <div className="sapphire-reactor__head sapphire-reactor__head--overlay">
-          <span className="sapphire-label">Total Shards</span>
-          <span className="sapphire-reactor__shards sapphire-mono">{sapphireNumber(profile?.shards)}</span>
-        </div>
-
         <button
           type="button"
           className="sapphire-reactor__crystalBtn"
@@ -112,13 +110,15 @@ export default function MiningReactor({
             </div>
 
             <div className={`sapphire-reactor__crystalLive ${active ? "sapphire-reactor__crystalLive--tap" : ""}`}>
-              <img
-                className="sapphire-reactor__crystalImg"
-                src={crystalImg}
-                alt=""
-                draggable={false}
-              />
-              <div className="sapphire-reactor__shineSweep" aria-hidden="true" />
+              <div className="sapphire-reactor__crystalMask">
+                <img
+                  className="sapphire-reactor__crystalImg"
+                  src={crystalImg}
+                  alt=""
+                  draggable={false}
+                />
+                <div className="sapphire-reactor__shineSweep" aria-hidden="true" />
+              </div>
             </div>
 
             {bursts.map((burst) =>
